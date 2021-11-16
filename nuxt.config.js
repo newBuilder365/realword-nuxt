6c7d9052eb7d1fa5f4ac48a1703cd84ca712c2b5
newbuilder365 <772512900@qq.com>
2021-11-16 22:47:46 +0800

    realword-nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.splice(0)
+      routes.push(...[{
+        path: '/',
+        component: resolve(__dirname, 'pages/layouts'),
+        children: [
+          {
+            path: '',
+            component: resolve(__dirname, 'pages/home'),
+            name: 'HomePage'
+          },
+          {
+            path: '/login',
+            component: resolve(__dirname, 'pages/login'),
+            name: 'LoginPage'
+          },
+          {
+            path: '/register',
+            component: resolve(__dirname, 'pages/login'),
+            name: 'RegisterPage'
+          },
+          {
+            path: '/profile/:username',
+            component: resolve(__dirname, 'pages/profile'),
+            name: 'ProfilePage'
+          },
+          {
+            path: '/settings',
+            component: resolve(__dirname, 'pages/settings'),
+            name: 'SettingsPage'
+          },
+          {
+            path: '/article/:slug',
+            component: resolve(__dirname, 'pages/article'),
+            name: 'ArticlePage'
+          },
+          {
+            path: '/editor',
+            component: resolve(__dirname, 'pages/editor'),
+            name: 'EditorPage'
+          }
+        ]
+      }])
+    }
+  }
+}
